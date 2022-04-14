06f7bb9d52bc0d3088d7f2925c7850cc761432fb
Akr0nys <55049058+larix45@users.noreply.github.com>
2022-04-14 13:00:34 +0200

    v2

--- content-script.js
+++ content-script.js
@@ -1,39 +1,51 @@
 let extensionWorldJsLogValue = null
-
+let extensionWorldBannedIpAddressess = ['"52.29.128.240"']
 document.getElementsByTagName("body")[0].onload = () => {
     setTimeout(()=>{
-        extensionWorldJsLogValue = document.getElementsByTagName("body")[0].getAttribute("jsLog")
-        setInterval(() => {
-            if(extensionWorldJsLogValue != document.getElementsByTagName("body")[0].getAttribute("jsLog")) // zmiana emaila
-            {
-                let emailDomain = document.getElementsByClassName("gD")[0].innerHTML;
-                let finalEmailDomain = emailDomain.substring(emailDomain.indexOf("@")+1)
-                finalEmailDomain = finalEmailDomain.substring(0, finalEmailDomain.indexOf("<"))
-                console.log(finalEmailDomain)
-                fetch(encodeURI("http://localhost:23144/dns-check"), 
+        try{
+            extensionWorldJsLogValue = document.getElementsByTagName("body")[0].getAttribute("jsLog")
+            setInterval(() => {
+                if(extensionWorldJsLogValue != document.getElementsByTagName("body")[0].getAttribute("jsLog")) // zmiana emaila
                 {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: `{"domain":"${finalEmailDomain}"}`
-                })
-                .then(response => {
-                    response.text().then((e)=>{
-                        console.log(e)
-                        if (e == '"52.29.128.240"')
-                        {
-                            console.log("SPAM")
-                            alert("Prawdopodonie test od SECAWA!")
-                        }
+                    document.querySelector("img.ajz").click()
+                    document.querySelector("img.ajz").click()
+                    let emailDomain =  document.querySelectorAll("div.ajB.gt table.ajC tbody tr.ajv td.gL span.gI")[5].innerHTML
+                    if(! new RegExp("^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$").test(emailDomain))
+                    { 
+                        emailDomain =  document.querySelectorAll("div.ajB.gt table.ajC tbody tr.ajv td.gL span.gI")[4].innerHTML
+                    }
+                    //document.getElementsByClassName("gD")[0].innerHTML;
+                    //let finalEmailDomain = emailDomain.substring(emailDomain.indexOf(">")+1)
+                    //finalEmailDomain = finalEmailDomain.substring(0, finalEmailDomain.indexOf("<"))
+                    console.log(emailDomain)
+                    fetch(encodeURI("http://localhost:23144/dns-check"), 
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: `{"domain":"${emailDomain}"}`
                     })
-                })
-                .catch(error => {
-                    console.log("error");
-                });
-
-                extensionWorldJsLogValue = document.getElementsByTagName("body")[0].getAttribute("jsLog")
-            }
-        }, 500)
+                    .then(response => {
+                        response.text().then((e)=>{
+                            console.log(e)
+                            if (extensionWorldBannedIpAddressess.includes(e))
+                            {
+                                alert("Prawdopodonie test od SECAWA!")
+                            }
+                        })
+                    })
+                    .catch(error => {
+                        console.log("error");
+                    });
+    
+                    extensionWorldJsLogValue = document.getElementsByTagName("body")[0].getAttribute("jsLog")
+                }
+            }, 500)
+        }catch(error)
+        {
+            console.log(error)
+        }
+        
     }, 1000)
 }
